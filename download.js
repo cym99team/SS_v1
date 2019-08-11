d14f2528edf379d909898895862b29b911ba2c4e
sdew2-0 <sdewcademy@g.ncu.edu.tw>
2019-08-11 21:29:30 +0800

    headhead OWO/

--- download.js
+++ download.js
@@ -1,13 +1,24 @@
 import React from 'react';
 //import React, {Component} from 'react';
 // import * as FileSystem from 'expo-file-system';
-import { StyleSheet, Text, View, Button, CameraRoll} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  CameraRoll,
+  TouchableOpacity,
+  Image,
+  Dimensions,
+} from 'react-native';
 import Video from 'react-native-video';
 import RNFS from 'react-native-fs';
 import Share from 'react-native-share';
 // import RNFetchBlob from "rn-fetch-blob";
 // import Video from 'react-native-af-video-player'
 // import { Permissions } from 'expo-permissions';
+
+var width = Dimensions.get('window').width;
+var height = Dimensions.get('window').height;
 
 export default class Download extends React.Component {
   state = {
@@ -23,136 +34,185 @@ export default class Download extends React.Component {
     //let { image } = this.state;
 
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' ,backgroundColor: 'skyblue'}}>
-        
-        {//image &&
-          //<Image source={{ uri: image }} style={{ width: 250, height: 250 }} />} 
+
+      <View style={styles.container}>      
+        {/* //<Image source={{ uri: image }} style={{ width: 250, height: 250 }} />}  */}
         <Video
-					source={{ uri:'http://140.115.87.141:9888/static/'+global.textname }} //http://techslides.com/demos/sample-videos/small.mp4
-					// url = {'http://140.115.87.141:9888/static/'+global.textname}
-					autoplay
-					repeat={true}
+          source={{ uri:'http://140.115.87.141:9888/static/'+global.textname }} //http://techslides.com/demos/sample-videos/small.mp4
+          // source={{ uri:'http://techslides.com/demos/sample-videos/small.mp4' }}
+          // url = {'http://140.115.87.141:9888/static/'+global.textname}
+          // source={require('./Image/V_20190810_233541_vHDR_Auto.mp4')}
+          // source={require('./Image/small.mp4')}
+          autoplay
+          repeat={true}
           rate={1.0}
           volume={1.0}
           isMuted={false}
-          // resizeMode="cover"
+          resizeMode="contain"
           // shouldPlay
           // isLooping//width: 400, height: 270 ,
-          style={{ width: 300, height: 270 }}/>}
-          
-        <Button
-          title="Download"
-          onPress={this.downloadFile}
-        />  
-        <Button
-          title="share to instagram"
-          onPress={this.ShareToIG}
-        />
-      
+          style={{
+            width: width,
+            height: height,
+            position: 'absolute',
+            // resizeMode:'contain',
+          }}
+        />  {/* end of Video*/}
+        
+        <TouchableOpacity onPress={() => this.ShareToIG()}
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}>
+          {/* <TouchableOpacity onPress={this.selectVideoTapped.bind(this)}> */}
+          <View style={[styles.buttonstyle]}>
+            <Image style={{ height: width * 0.05, width: width * 0.05, margin: 10 }} source={require('./Image/instagram.png')} />
+            {/* <Text style={{ fontSize: 15, fontWeight: 'bold', color: '#fff' }}>Share</Text> */}
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => this.ShareToFB()}
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}>
+          {/* <TouchableOpacity onPress={this.selectVideoTapped.bind(this)}> */}
+          <View style={[styles.buttonstyle]}>
+            <Image style={{ height: width * 0.05, width: width * 0.05, margin: 10 }} source={require('./Image/facebook.png')} />
+            {/* <Text style={{ fontSize: 15, fontWeight: 'bold', color: '#fff' }}>Download Video</Text> */}
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => this.downloadFile()}
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}>
+          {/* <TouchableOpacity onPress={this.selectVideoTapped.bind(this)}> */}
+          <View style={[styles.buttonstyle]}>
+            <Image style={{ height: width * 0.05, width: width * 0.05, margin: 10 }} source={require('./Image/download_white.png')} />
+            {/* <Text style={{ fontSize: 15, fontWeight: 'bold', color: '#fff' }}>Download Video</Text> */}
+          </View>
+        </TouchableOpacity>
+
       </View>
-      
+
     );
   }
-  
+
 
   downloadFile() {
-    // alert('download!!!!!!!!!!!!!!');
     // const downloadDest = RNFS.DocumentDirectoryPath+'/'+global.textname;
     // RNFS.mkdir( RNFS.DocumentDirectoryPath + '/favorite/')
     // const downloadDest ='file://'+RNFS.DocumentDirectoryPath+'/'+global.textname;
-    const downloadDest ='file://'+RNFS.TemporaryDirectoryPath+'/'+global.textname;
-    // alert('file://'+RNFS.TemporaryDirectoryPath+'/'+global.textname);
-    const formUrl = 'http://140.115.87.141:9888/static/'+global.textname;
+    const downloadDest = 'file://' + RNFS.TemporaryDirectoryPath + '/' + global.textname;
+    const formUrl = 'http://140.115.87.141:9888/static/' + global.textname;
     const options = {
       fromUrl: formUrl,
       toFile: downloadDest,
       background: true,
-    // begin: (res) => {
-    // console.log('begin', res);
-    // // console.log('contentLength:', res.contentLength / 1024 / 1024, 'M');
-    // },
-    //   progress: (res) => {
-    //   let pro = res.bytesWritten / res.contentLength;
-    //     this.setState({
-    //       progressNum: pro,
-    //     });
-    //   }
-    }; // end options
-      try {
-        // alert('ddddddd');
+      // begin: (res) => {
+      // console.log('begin', res);
+      // // console.log('contentLength:', res.contentLength / 1024 / 1024, 'M');
+      // },
+      //   progress: (res) => {
+      //   let pro = res.bytesWritten / res.contentLength;
+      //     this.setState({
+      //       progressNum: pro,
+      //     });
+      //   }
+    }; 
+
+    try {
       RNFS.downloadFile(options).promise
-      .then(res => {
-        // this.saveVideo(res.uri);
-        alert('success');
-        // alert(downloadDest);
-      CameraRoll.saveToCameraRoll(downloadDest)
-                          .then(()=>{
-                              alert('圖片已保存到圖片庫')
-                          }).catch(()=>{
-                              alert('圖片保存失敗')
-                          })
-                          
-      })
-      .catch(err => {
-        console.log('err', err);
+        .then(res => {
+          // this.saveVideo(res.uri);
+          alert('success');
+          // alert(downloadDest);
+          CameraRoll.saveToCameraRoll(downloadDest)
+          .then(() => {
+            alert('圖片已保存到圖片庫')
+          }).catch(() => {
+            alert('圖片保存失敗')
+          })
+
+        })
+        .catch(err => {
+          console.log('err', err);
         });
-      }
-      catch (e) {
+    } catch (e) {
       console.log(e);
-      }
+    }
   }
 
-  // downloadFile = async () => {
-	// 	alert("download!!")
-	// 	// this.state.videoUri = 'http://140.115.87.141:9888/static/'+global.textname;
-	// 	// alert(this.state.videoUri);
-  //   // if (this.state.videoUri) {
-  //   //     this.saveVideo(this.state.videoUri);
-  //   // }
-
-  // } 
-    ShareToIG() {
-
+  ShareToIG() {
     const shareOptions = {
       title: 'Share via',
       // message: 'some message',
-      url: 'file://'+RNFS.TemporaryDirectoryPath+'/'+global.textname,
-      social: Share.Social.FACEBOOK
+      url: 'file://' + RNFS.TemporaryDirectoryPath + '/' + global.textname,
+      social: Share.Social.INSTAGRAM,
     };
     // Share.open(shareOptions)
     // .then((res) => { console.log(res) })
     // .catch((err) => { err && alert(err); });
     Share.shareSingle(shareOptions);
-    
-    };
+  };
 
+  ShareToFB() {
+    // if(RNFS.exists(filepath))
+    //     s=" exists.";
+    // else
+    //     s=" doesn't exist.";
+    // alert(filepath + s);
+
+    const shareOptions = {
+      title: 'Share via',
+      // message: 'some message',
+      url: 'file://' + RNFS.TemporaryDirectoryPath + '/' + global.textname,
+      social: Share.Social.FACEBOOK,
+    };
+    // Share.open(shareOptions)
+    // .then((res) => { console.log(res) })
+    // .catch((err) => { err && alert(err); });
+    Share.shareSingle(shareOptions);
+  };
+  
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    backgroundColor: '#000',
+  },
+  logo: {
+    flex: 1,
+    justifyContent: 'center',
+    // alignItems: 'center',
+  },
+  buttonstyle: {
+    // flexDirection: 'row',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    // borderColor: '#5c82cf',
+    borderColor:'#fff',
+    alignItems: 'center',
+    // backgroundColor: '#F5FCFF',
+    backgroundColor: 'rgba(0,0,0,0.3)',
+    // borderWidth: 1,
+    borderRadius: 999,
+    padding: 10,
+    // paddingHorizontal: 20,
+    marginBottom: 10,
+    // width: '60%',
+    // margin: 30,
+    // height: 80,
+    // width: 150
 
-//////////////////////////
-//     // PickUp.sendname()
-//     // DOWNLOAD VIDEO 
-//     const videoPath = FileSystem.documentDirectory+global.textname;
-//     console.log("1-"+videoPath);//http://techslides.com/demos/sample-videos/small.mp4
-//     const resvideo = await FileSystem.downloadAsync('http://140.115.87.141:9888/static/'+global.textname, videoPath);
-//     console.log("2-"+resvideo);
-//     const status ='granted';
-//     //console,log('video');
-//     if (status === 'granted') {
-//       try{
-//         //console.log("prima "+resvideo.uri);
-//         const resultvideo = await CameraRoll.saveToCameraRoll(resvideo.uri,'video');
-        
-//         console.log("CAMERAROLL = "+resultvideo)
-//         //this.props.navigation.navigate('cam')
-//       }
-//       catch(e){
-//         console.log("err");
-//         console.warn(JSON.stringify(e))
-//       }
-//     }
-//     console.log("done")
-    
-   
-
-
+  }
+});
--- pickup.js
+++ pickup.js
@@ -1,166 +1,289 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ImagePicker from 'react-native-image-picker';
 // import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 // var ImagePicker = require('react-native-image-picker');
 
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  PixelRatio,
-  TouchableOpacity,
-  Image,
+	Platform,
+	StyleSheet,
+	Text,
+	View,
+	PixelRatio,
+	TouchableOpacity,
+	Image,
+	Modal,
+	Dimensions,
 
 } from 'react-native';
-//import SplashScreen from 'rn-splash-screen';
-
+import Button from 'react-native-share/components/Button';
+import Spinkiter from 'react-native-spinkit';
+const visible = null;
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+	ios: 'Press Cmd+R to reload,\n' +
+		'Cmd+D or shake for dev menu',
+	android: 'Double tap R on your keyboard to reload,\n' +
+		'Shake or press menu button for dev menu',
 });
 
+var width = Dimensions.get('window').width;
+var height = Dimensions.get('window').height;
+
 export default class pick extends React.Component {
-    
-    //   componentDidMount() {
-    //     setTimeout(() => {
-    //       SplashScreen.hide();
-    //     }, 2000);
-    //   }
 
-    //選擇影片
-    selectVideoTapped() {
+	// componentDidMount() {
+	// 	setTimeout(() => {
+	// 		SplashScreen.hide();
+	// 	}, 2000);
+	// }
 
-        var state = {
-            avatarSource: null,
-            video: null
-        };
+	state = {
+		modalVisible: false,
+	};
 
-        const options = {
-            
-            title: '選擇影片',
-            cancelButtonTitle: '取消',
-            takePhotoButtonTitle: '錄製',
-            chooseFromLibraryButtonTitle: '從圖庫',
-            mediaType: 'video',
-            videoQuality: 'medium',
+	//選擇影片
+	selectVideoTapped() {
 
-            storageOptions:{
-                skipBackup:true,
-                path:'images'
-            }
-        };
+		var state = {
+			avatarSource: null,
+			video: null,
+		};
 
-        ImagePicker.showImagePicker(options, (response) => {
+		const options = {
+			title: 'Select Video To Upload',
+			cancelButtonTitle: 'cancel',
+			takePhotoButtonTitle: 'RECORD',
+			chooseFromLibraryButtonTitle: 'From Library',
+			mediaType: 'video',
+			videoQuality: 'high',
 
-            console.log('Response = ', response);
+			storageOptions: {
+				skipBackup: true,
+				path: 'images'
+			}
+		};
 
-            if (response.didCancel) {
-                console.log('User cancelled video picker');
-            }
-            else if (response.error) {
-                console.log('Error: ', response.error);
-            }
-            else {
-                this.setState({
-                    video: response
-                });
-                // this.props.actions.UploadVideo();
-            
-            let video = this.state.video;
-            let target_url  = 'http://140.115.87.141:9888/';
-            let localUri = video.uri;
-            let filename = localUri.split('/').pop()+'.mp4';
-            
-            console.log('\n\n\n');
-            console.log(video);
-            console.log('\n\n\n');
+		ImagePicker.showImagePicker(options, (response) => {
 
-            let formData = new FormData();
+			console.log('Response = ', response);
 
-                formData.append('file', {
-                    name: filename,
-                    type: 'video/mp4',
-                    uri: video.uri,
-                    // .replace("file://", ""),     
-                });
-
-                fetch(target_url,{
-                    method: 'POST',
-                    // body: JSON.stringify(formData),
-                    body: formData,
-                    headers: {
-                        // "Accept": "application/json",
-                        // "Content-Type": 'application/json',      
-                    'Content-Type': 'multipart/form-data'
-                    }
-                })
-                .then(response => {
-                console.log("upload success\n", response);
-                alert("Upload success!");
-                
-                //console.log('888')
-                return response.text()
-                
-                })
-                .catch(error => {
-                console.log("\n\n\nupload error\n\n\n", error);
-                    alert("Upload failed!");
-                }).then(textData => {
-                    this.setState({ name: textData })
-                    //download.setState({name: textData})
-                    console.log(textData);
-                    textname = textData;
-                    //this.setState({ success:true})
-                    //console.log(this.state.success);
-                    //alert("okkkkk");
-                    this.props.navigation.navigate('Load')
-                })
-            }
-            
-        });
-        
-    }
+			if (response.didCancel) {
+				console.log('User cancelled video picker');
+			}
+			else if (response.error) {
+				console.log('Error: ', response.error);
+			}
+			else {
+				this.state.modalVisible = true;
+				this.setState({
+					video: response
+				});
 
 
-    render() {
-        return (
-            
-            <View style={styles.container}>          
-                <TouchableOpacity onPress={() => this.selectVideoTapped()}>
-                {/* <TouchableOpacity onPress={this.selectVideoTapped.bind(this)}> */}
-                    <View style={[styles.avatar, styles.avatarContainer]}>
-                        <Text>選擇影片</Text> 
-                    </View>
-                </TouchableOpacity>
+				let video = this.state.video;
+				let target_url = 'http://140.115.87.141:9888/';
+				let localUri = video.uri;
+				let filename = localUri.split('/').pop() + '.mp4';
 
-        
-            </View>
-        );
-    }
+				console.log('\n\n\n');
+				console.log(video);
+				console.log('\n\n\n');
+
+				let formData = new FormData();
+
+				formData.append('file', {
+					name: filename,
+					type: 'video/mp4',
+					uri: video.uri,
+					// .replace("file://", ""),     
+				});
+
+				fetch(target_url, {
+					method: 'POST',
+					// body: JSON.stringify(formData),
+					body: formData,
+					headers: {
+						// "Accept": "application/json",
+						// "Content-Type": 'application/json',      
+						'Content-Type': 'multipart/form-data'
+				  }	
+				})
+				.then(response => {
+					console.log("upload success\n", response);
+
+					alert("Upload success!");
+					this.state.modalVisible = false;
+					//console.log('888')
+					return response.text()
+				})
+				.catch(error => {
+					console.log("\n\n\nupload error\n\n\n", error);
+					alert("Upload failed!");
+				})
+				.then(textData => {
+					this.setState({ name: textData })
+
+					//download.setState({name: textData})
+					console.log(textData);
+					textname = textData;
+					//this.setState({ success:true})
+					//console.log(this.state.success);
+					//alert("okkkkk");
+
+					this.props.navigation.navigate('Load')
+				})
+			}
+
+		});
+
+  }
+
+
+	render() {
+		return (
+
+			<View style={styles.container}>
+
+				<Modal
+					visible={this.state.modalVisible}
+					animationType="fade"
+					transparent={true}>
+					<View style={styles.container}>
+						<Text style={{ fontSize: 20, fontWeight: 'bold' }} >Loading</Text>
+						<Text style={{ fontSize: 20, fontWeight: '100' }} >- =͟͟͞͞ =͟͟͞͞ = ﾍ( ´Д`)ﾉ</Text>
+						<Spinkiter isVisible={true} size={80} type={'ThreeBounce'} color={"#000000"} />
+					</View>
+				</Modal>
+
+
+				{/* <View style={{
+					// flex: 1,
+					flexDirection: 'column',
+					alignItems: 'center',
+					justifyContent: 'center',
+				}
+				}> */}
+
+				{/* <Image style={{ width: width * 0.6, height: width * 0.6 }} source={require('./Image/logo.png')} /> */}
+				<Image style={{
+					width: width*1.3,
+					height: height*1.3,
+					position: 'absolute',
+					top: -height*0.25,
+				}} source={require('./Image/main.png')} />
+				{/* <Image style={{width:200, height:200}} source={require('./Image/logo.png')}/> */}
+
+
+				<View style={{
+					// flex: 1,
+					flexDirection: 'column',
+					justifyContent: 'space-around',
+					alignItems: 'center',
+					// borderColor: '#000',
+					// borderWidth: 1,
+					marginBottom: height * 0.15,
+
+				}}>
+					<Text style={{
+						fontSize: 15,
+						fontWeight: 'bold',
+						color: '#fff',
+						marginBottom: 20,
+					}}>Welcome to Stunning !</Text>
+							
+					<TouchableOpacity onPress={() => this.selectVideoTapped()}
+						style={{
+							flexDirection: 'row',
+							justifyContent: 'center',
+							alignItems: 'center'
+						}}>
+						{/* <TouchableOpacity onPress={this.selectVideoTapped.bind(this)}> */}
+						<View style={[styles.buttonstyle,{
+							backgroundColor:'#fff'
+						}]}>
+							<Image style={{
+								height: width * 0.05,
+								width: width * 0.05,
+								margin: 10,
+								opacity:0.5,
+							}} source={require('./Image/video-camera_black.png')} />
+							<Text style={{
+								fontSize: 15,
+								fontWeight: 'bold',
+								color: '#999'
+							}}>Choose Video</Text>
+						</View>
+					</TouchableOpacity>
+
+					<TouchableOpacity onPress={() => this.selectVideoTapped()}
+						style={{
+							flexDirection: 'row',
+							justifyContent: 'center',
+							alignItems: 'center'
+						}}>
+						<View style={[styles.buttonstyle]}>
+							<Image style={{
+									height: width * 0.05,
+									width: width * 0.05,
+									margin: 10
+							}} source={require('./Image/dance.png')} />
+							<Text style={{
+									fontSize: 15,
+									fontWeight: 'bold',
+									color: '#fff'
+							}}>Pose Demo</Text>
+						</View>
+					</TouchableOpacity>
+				</View>
+			</View>           
+		);
+	}
 
 }
 
 
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF'
-    },
-    avatarContainer: {
-        borderColor: '#9B9B9B',
-        borderWidth: 1 / PixelRatio.get(),
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    avatar: {
-        borderRadius: 50,
-        width: 100,
-        height: 100
-    }
+	container: {
+		flex: 1,
+		justifyContent: 'flex-end',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF'
+	},
+
+	logo: {
+		flex: 1,
+		justifyContent: 'center',
+		// alignItems: 'center',
+	},
+	// avatarContainer: {
+	//     borderColor: '#9B9B9B',
+	//     borderWidth: 1 / PixelRatio.get(),
+	//     justifyContent: 'center',
+	//     alignItems: 'center'
+	// },
+	// avatar: {
+	//     borderRadius: 50,
+	//     width: 100,
+	//     height: 100
+	// },
+	buttonstyle: {
+		// flexDirection: 'row',
+		flexDirection: 'row',
+		justifyContent: 'center',
+		// borderColor: '#5c82cf',
+		borderColor:'#fff',
+		alignItems: 'center',
+		// backgroundColor: '#F5FCFF',
+		backgroundColor: 'transparent',
+		borderWidth: 1,
+		borderRadius: 999,
+		padding: 10,
+		paddingHorizontal: 20,
+		marginBottom: 12,
+		width: '60%',
+		// margin: 30,
+		// height: 80,
+		// width: 150
+	}
+
 });
